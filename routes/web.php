2cc18abb066635d841c4c62f071c6e5d63a2812d
MuhSaidAbbas <baseness1470@gmail.com>
2025-09-29 16:58:46 +0700

    Tugas Latihan Web Framework

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/home', function () {
+    return view ('home');
+});
+
+Route::get('/about', function () {
+    return view ('about');
+});
+
+Route::get('/program', function () {
+    return view ('program');
+});
+
+Route::get('/our_team', function () {
+    return view ('our_team');
+});
+
+
 Route::get('/welcome', function () {
     return view ('welcome');
 });
